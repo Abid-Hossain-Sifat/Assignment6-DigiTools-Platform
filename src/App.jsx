6a13295109006808397b9661d116cf74b1fb83b1
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-03-29 19:31:06 +0600

    Project Setup (React, TailwindCss, Daisy UI)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='text-red-400'>Hello PH</h1>
+    <button className='btn btn-primary'>
+      Click Now
+    </button>
+    </>
+  )
+}
+
+export default App
